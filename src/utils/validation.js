78f40ac3bcbe6788a850c5e93f9c1d41c4b51172
Lord-Y14 <68954094+Lord-Y14@users.noreply.github.com>
2022-03-30 11:21:42 +0200

    Putting the validation class in the right place

---
+++ src/utils/validation.js
@@ -0,0 +1,56 @@
+class validation{
+	//class for validation methods to be used when screening user input
+    // to use in other js files-> import{validation} from './validation.js';
+
+constructor(){} //mandatory constructor method 
+ static onlyDigits(sLine){//returns true if a string only contains numbers
+	var sChar;
+ for(let i = 0;i<sLine.length;i++){
+	sChar = sLine[i];
+	if( !(sChar >= "0" && sChar<="9") ){
+		return false;
+	}
+  }
+  return true;
+ }
+
+ static validEmail(sEmail){ //returns true if a string contains only 1 "@"  and at least 1 "." //maybe restrict email domain in the future?
+	var flag1 = false;
+	var flag2 = false;
+	var sChar;
+	for(let i = 0;i<sEmail.length;i++){
+		if(sEmail[i] == "@"){
+			if(flag1 == false){
+				flag1 = true;
+			}
+			else{
+				return false;
+			}
+		}
+		if(flag2 | sEmail[i] == "."){
+			flag2 = true;
+		}
+	}
+	if(flag1 && flag2){
+		return true;
+	}
+	else{
+		return false;
+	}
+	
+ }
+
+ static validPhoneNumber(sPhoneNumber){ //returns true if a number is exactly 10 digits and contains only numbers
+	var a = sPhoneNumber;
+	a = a.replace(" ","");
+	if( ( a.length == 10) &&onlyDigits(a)){
+		return true;
+	}else{
+		return false;
+	}
+ }
+}
+
+
+
+

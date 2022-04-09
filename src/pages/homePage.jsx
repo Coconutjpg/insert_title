5fd91dfb1ae97d50708d8a6c8ea0f7cbb99d1ced
PostMillennial <54679328+PostMillennial@users.noreply.github.com>
2022-04-09 13:32:39 +0200

    failed select element

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,7 +1,5 @@
 import React from "react"
 import { getProductsByCategory } from "../utils/database_functions";
-import { element } from "prop-types";
-import Card from "../components/card";
 import "../stylesheets/style.css"
 import Carousel from "../components/carousel";
 import Products from "../components/products";
@@ -10,25 +8,8 @@ export default class HomePage extends React.Component{
 
     state = {
         items : [],
-        req_complete : false
-    }
-
-    items = []
-
-    // generates an item list retrieved from the db
-    getlist = (catagory) =>{
-        const cat = catagory; // take input
-        if(this.state.req_complete) return;
-
-        let prods = getProductsByCategory(cat);// assign this way just to be safe 
-        //As products_in_categories_ is dependent on the async function, a promise is returned, 
-        //thus we need to resolve that promise to get access to what was returned in the asynchronous function
-        Promise.resolve(prods).then((arr)=>{
-            this.items = arr
-            this.setState({
-                req_complete : true
-            })
-        })
+        req_complete : false.valueOf,
+        category : "All"
     }
 
      // keeps track of values that change on the DOM
@@ -42,42 +23,29 @@ export default class HomePage extends React.Component{
             [name]: value
         })
         
-        console.log(this.state);
+        console.log(value);
     }
     renderSwitch(cat) {
+        console.log("cat = " + cat)
         switch(cat) {
-          case 'All':
-           return( 
-           <div>
-            <Products category="Graphics_Cards"/> 
-            <Products category="Monitors"/>
-            </div>
-            );
-          case 'Graphics_cards':
-            return( 
-                <div>
-                 <Products category="Graphics_Cards"/> 
-                 </div>
-                 );
-          case 'Monitors':
-            return( 
-                <div>
-                 <Products category="Graphics_Cards"/> 
-                 </div>
-                 );
-          default:
-            return( 
-                <div>
-                 <Products category="Graphics_Cards"/> 
-                 <Products category="Monitors"/>
-                 </div>
-                 );;
+            case "All":
+                return( 
+                    <div>
+                        <Products category="Graphics_Cards"/>
+                        <Products category="Monitors"/>
+                    </div>
+                );
+            break;
+            default :
+                return(
+                    <Products category={cat}/>
+                )
+            break
+
+            
         }
       }
     render(){
-        this.getlist("Graphics_Cards");
-        //alert(this.state.items)
-
         return(
             
             <div>
@@ -93,25 +61,15 @@ export default class HomePage extends React.Component{
                 </select>
                 {/*onChange={this.categorySelect()} */}
                 <select name="category" id="categorySelect" onChange={this.handleInputChange} >
-                    <option value="All">All Categories</option>
-                    <option value="Graphics_Cards">Graphics Cards</option>
-                    <option value="Monitors"> Monitors</option>
+                    <option value={"All"}>All Categories</option>
+                    <option value={"Graphics_Cards"}>Graphics Cards</option>
+                    <option value={"Monitors"}>Monitors</option>
                 </select>
 
             </div>
-            {/*  <Carousel/> */}   
-              {/* <Products/> */}
-          {/*  {
-                if(document.getElementById("categorySelect").value== "All"){
-                   console.log("all elments selected");
-                }
-            }     */} 
-           {/* <Products category="Graphics_Cards"/> 
-            <Products category="Monitors"/>*/}
-            {this.renderSwitch(this.state.categorySelect)}
-                        
-              
- 
+
+            {this.renderSwitch(this.state.category)}  
+
             </div>
 
         );

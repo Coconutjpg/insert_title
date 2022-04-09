ed9068552178b955298cedf917ac4b7e99c44851
AmaxCoder <78669694+AmaxCoder@users.noreply.github.com>
2022-04-09 10:02:23 +0200

    Created login backend functionality for frontend
    performLogin is used to take login details and authenticate with the database.
    validateEmail will check for "@" and "." characters in email.
    validatePassword will check password length.

--- src/utils/loginUtils.js
+++ src/utils/loginUtils.js
@@ -1,4 +1,45 @@
-/**
-* String validation
-* Passing 
+/** 
+* Needed by Frontend to login
+* database login authentication
+* string validation
 */
+
+import {logIn} from "./database_functions"
+import{validation} from './validation.js'
+
+
+/* 
+performLogin
+
+input: JSON object with emailAddress and password
+
+ouput: if login is successful, returns size 2 array of the string "success" and user details in a JSON Object,
+       otherwise an alert is thrown
+*/
+function performLogin(details){  
+ 
+     let l = logIn(details.emailAddress,details.password);
+     Promise.resolve(l).then((result) =>{
+         if(result[0]==="success"){
+ 
+             return result[1];  // take result[1] (user details) and pass it to the homepage 
+         }
+         else{      //reset textfields
+        
+            alert("incorrect details");
+
+         }
+     })
+
+}
+
+function validateEmail(details){     //returns true if a string contains only 1 "@"  and at least 1 "."
+   return validation.validEmail(details.emailAddress); 
+}
+
+function validatePassword(details){  //returns true if the password contains at least 6 characters
+    return validation.validPassword(details.password);
+ }
+
+
+export {performLogin,validatePassword,validateEmail};

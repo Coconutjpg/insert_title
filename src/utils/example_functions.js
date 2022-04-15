09ac1fb237c298c7c93e519b0a1542f9b29237b1
NoName-0512 <92608156+NoName-0512@users.noreply.github.com>
2022-04-15 16:28:39 +0200

    Added credit and clicking examples

--- src/utils/example_functions.js
+++ src/utils/example_functions.js
@@ -1,15 +1,7 @@
-// Example file to show how the functions are used to help with the backend implementation
-
 import {
-  getProductsByCategory, getCategories ,signUp, logOut, logIn
+  getProductsByCategory, getCategories ,signUp, logOut, logIn, getProductsWithSorting_Limits_Category, getCredits,addCredits, clicked
 } from "./firebase.js"
 
-
-//logging out
-const logoutButton = document.querySelector('.logout')
-logoutButton.addEventListener('click',()=>{
-logOut()
-})
 
 //Get all the categories
 let categories = getCategories()
@@ -24,6 +16,12 @@ Promise.resolve(products_in_categories_).then((arr)=> console.log(arr))
 //Gets the products based on query inputs
 let products_with_sorts = getProductsWithSorting_Limits_Category("Graphics_Cards",'prod_cost','asc',10000,10)
 Promise.resolve(products_with_sorts).then((arr)=>console.log(arr))
+
+//logging out
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click',()=>{
+logOut()
+})
 
 //signing up users
 const signupForm  = document.querySelector('.signup')
@@ -43,7 +41,6 @@ Promise.resolve(succ).then((ret)=>{
   //When the signup is successful
   if(ret[0]==="success"){
     console.log("successful")
-    //When the signUp is successful the user json object will be placed into the second element of the array returned
     console.log(ret[1])
   }
   //When the signup is unsuccessful
@@ -65,11 +62,43 @@ let succ = logIn(email,password)
 Promise.resolve(succ).then((ret)=>{
   if(ret[0]==="success"){
     console.log("successful")
-    //When the login is successful the user json object will be placed into the second element of the array returned
     console.log(ret[1])
   }
   else{
     console.log('unsuccessful')
   }
+
+
 })
+
+})
+
+//Getting a user's credits
+const getusersCreditsButton = document.querySelector('.get_users_credits')
+getusersCreditsButton.addEventListener('click',()=>{
+
+  const credits = getCredits("duranreddy@gmail.com")
+  Promise.resolve(credits).then((ret)=>{
+    console.log(ret)
+  })
+
+})
+
+//Adding credits to a user
+const add_Credits = document.querySelector('.add_credit')
+add_Credits.addEventListener('click',()=>{
+  const add_credits = addCredits("duranreddy@gmail.com",1000)
+  Promise.resolve(add_credits).then((ret)=>{
+    console.log(ret)
+  })
+
+})
+
+//Clicked a product
+const clicked_product = document.querySelector('.clicked_product')
+clicked_product.addEventListener('click',()=>{
+    const prod_clicked = clicked('duranreddy@gmail.com',"FR7sF3vF6NiH6xuItNrs")
+    Promise.resolve(prod_clicked).then((ret)=>{
+      console.log(ret)
+    })
 })

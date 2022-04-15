bcff6347c932463f48b477ec9b1e8e4b138ae0aa
Lord-Y14 <yoshailenmichael@gmail.com>
2022-04-15 17:43:03 +0200

    fixed bugs in registration and implemented new db signup method

--- src/utils/registration.js
+++ src/utils/registration.js
@@ -28,29 +28,29 @@ function register(fName,lName,sDob,sEmail,sCell,sPassword,repPassword, onSucceed
 		error = error + "The length of your password needs to be atleast 6 characters long" + "\n";
 	}
 	//validate first name
-	if(!validation.onlyLetters(fName)){
+	if(!validation.onlyLetters(fName) | (fName.length == 0)){
 		flag = false;
 		error = error + "First Name should contain only alphabetical letters" + "\n";
 	}
 	//validate last name
-	if(!validation.onlyLetters(lName)){
+	if(!validation.onlyLetters(lName) | (lName.length == 0)){
 		flag = false;
 		error = error + "Last Name should contain only alphabetical letters" + "\n";
 	}
 	//validate email address
-	if(!validation.validEmail(sEmail)){
+	if(!validation.validEmail(sEmail) | (sEmail.length == 0)){
 		flag = false;
 		error = error + "Please enter a valid email address" + "\n";
 	}
 	//validate cell number
-	if(!validation.validPhoneNumber(sCell)){
+	if(!validation.validPhoneNumber(sCell) | (sCell.length == 0 )){
 		flag = false;
 		error = error + "Your phone number should be either 10 digits or 11 digits long" + "\n";
 	}
 	//validate user date of birth 
 	var d = new Date();
 	var user_dob = new Date(sDob);
-	if((d.getYear() - user_dob.getYear() < 16) | (d.getYear() - user_dob.getYear >80)){
+	if((d.getYear() - user_dob.getYear() < 16) | (d.getYear() - user_dob.getYear >80) | (sDob.length == 0)){
 	 flag = false;
 	 error = error + "Only users from 16 to 80 can register";
 	}
@@ -63,12 +63,9 @@ function register(fName,lName,sDob,sEmail,sCell,sPassword,repPassword, onSucceed
          if(ret[0]==="success"){
           console.log("user added");
           //When the signUp is successful the user json object will be placed into the second element of the array returned
-		  	setUser(ret[1])
-		  	onSucceed(ret[1])
             console.log(ret[1]);
 			alert("You have been successfully registered");
               }
-           
           else{ //When the signup is unsuccessful
               console.log("unable to add user");
 			  alert("Registration failed due to poor connection to database ");

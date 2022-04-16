56c961d0bb5c48ff3413f34f90f1a2454f4106da
AmaxCoder <78669694+AmaxCoder@users.noreply.github.com>
2022-04-16 20:19:26 +0200

    Added real-time on-page Validation to registration.jsx
    Added code to validate inputs and show errors on the page as user types.

--- src/pages/registration.jsx
+++ src/pages/registration.jsx
@@ -42,7 +42,7 @@ export default class RegistrationPage extends React.Component{
         const value = target.value;
         
         this.setState({
-            [name]: value
+            [name]: value,
         })
         
         //console.log(this.state);
@@ -53,61 +53,103 @@ export default class RegistrationPage extends React.Component{
             <React.Fragment>
                 <h1> Welcome To Registration </h1>
                 <form className="registerForm">
-                    
+
+
+                <div>    
                     <label>First Name:</label>
-                    <input classname="registerInput"
+                    <input className="registerInput"
                         name="firstName" 
                         type="string" 
                         placeholder="John"
+                        pattern = "^[A-Za-z]{1,}$"
+                        
                         onChange={this.handleInputChange}>
                     </input>
-                    
+                    <div></div>
+                    <span>First Name should contain alphabetical letters only</span>
+                </div>
+
+
+                <div>
                     <label>Last Name:</label>
                     <input 
                         name="lastName"
                         type="string" 
                         placeholder="Doe"
+                        pattern = "^[A-Za-z]{1,}$"
+                       
+
                         onChange={this.handleInputChange}/>
+                        <div></div>
+                         <span >Last Name should contain alphabetical letters only </span>   
+               </div>
 
 
+                <div>
                     <label>Date of Birth:</label>
                     <input 
                         type="date"
                         name="dob"
+                        max="2006-12-31" 
+                        min="1942-01-01"
                         onChange={this.handleInputChange}/>
-                    
+                        <div></div>
+                         <span >Only users from either 16 (turning 16 included) to 80 can register</span>
+                </div>
 
+
+                <div>
                     <label>Email Address:</label>
-
                     <input 
                         name="emailAddress"
-                        type="string" 
+                        type="email" 
                         placeholder="JD@fakeemail.com"
+                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
                         onChange={this.handleInputChange}>
                     </input>
-                    
-                    <label>Cellphone Number:</label>
+                    <div></div>
+                    <span > Email must be in the following order: characters@characters.domain </span>   
+                </div>
 
+
+                <div>
+                    <label>Cellphone Number:</label>
                     <input 
                         name="cellNo"
                         type="string" 
                         placeholder="050 5050 505"
+                        pattern = "[0-9]{10,11}$"
                         onChange={this.handleInputChange}>
                     </input>
+                    <div></div>
+                    <span > Your phone number should contain 10 digits or 11 digits only  </span>  
+
+                </div>    
+
 
                     <label>Password:</label>
-
+                <div>
                     <input 
                         name="password"
                         type="password"
-                        onChange={this.handleInputChange}/>                    
+                        pattern="^\S{6,}$"
+                        onChange={this.handleInputChange}/>  
+                    <div></div>
+                    <span >Password must contain 6 or more characters</span>  
+                </div>
 
+                
+                <div>
                     <label>Re-Enter Password:</label>
-
                     <input 
                         name="repeatPassword"
                         type="password"
+                        pattern="^\S{6,}$"
                         onChange={this.handleInputChange}/> 
+                        <div></div>
+                    <span >Password must contain 6 or more characters</span>  
+                </div>
+                        
                     
                     <input 
                         type="button" 

9c15e3aabfeb90f3ed07970013c54bf9191abbeb
dtjabring123 <101052704+dtjabring123@users.noreply.github.com>
2022-05-02 12:15:24 +0200

    Add files via upload

--- src/utils/hashing.js
+++ src/utils/hashing.js
@@ -1,21 +1,34 @@
-const bcrypt = require('bycrypt');
-const saltRounds = 13;
+export class hashing{
+    //class for password hashing
+    //to use in other js files -> import{hashing} from './hashing.js'
 
-function hashPassword(password){
-    bcrypt.genSalt(saltRounds,function(err,salt){
-        bcrypt.hash(password,salt,function(err,hash){
-            return hash
+    constructor(){
+        //mandatory constructor class
+    }
+
+    bcrypt = require('bycrypt');
+    saltRounds = 13; //number os salt rounds
+
+    //return hashed password
+    static hashPassword(password){
+        bcrypt.genSalt(saltRounds,function(err,salt){
+            bcrypt.hash(password,salt,function(err,hash){
+                return hash
+            })
+        });    
+    }
+
+    //compare password entered with hash, returns true if they match
+    static comparePassword(password,hash){
+        bcrypt.compare(password,hash,function(err,result){
+            if(result){
+                return true;
+            }
+            else{
+                return false;
+            }
         })
-    });
+    }
 }
 
-function comparePassword(password,hash){
-    bcrypt.compare(password,hash,function(err,result){
-        if(result){
-            return true;
-        }
-        else{
-            return false;
-        }
-    })
-}
+console.log(7)

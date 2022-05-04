025921ec8651a1525af4d2a0105fd247a6bee957
Lord-Y14 <yoshailenmichael@gmail.com>
2022-05-04 06:54:24 +0200

    fixed login toast messages

--- src/utils/loginUtils.js
+++ src/utils/loginUtils.js
@@ -16,35 +16,38 @@ input: JSON object with emailAddress and password
 ouput: if login is successful, returns size 2 array of the string "success" and user details in a JSON Object,
        otherwise an alert is thrown
 */
-function performLogin(details, onSuccess){  
+function performLogin(details){  
      console.log("email address is : " + details.emailAddress);
 	 var email = details.emailAddress;
 	 var password = details.password;
      if( (email.length == 0) |  !(validateEmail(email))  | !(validatePassword(password))  ){
 		//show toast message error here
+		console.log("caught invalid data");
 		var x = document.getElementById("snackbar");
         x.className = "show";
         x.innerHTML = "Invalid email address or password";
         setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-	 
-		 
-	 }else{ //try to log in 
+		
+	}
+	else{ //try to log in 
+	console.log("trying to log in");
      let l = logIn(details.emailAddress,details.password);
      Promise.resolve(l).then((result) =>{
          if(result[0]==="success"){
-            onSuccess(result[1])
-            return result[1];  // take result[1] (user details) and pass it to the homepage 
+		   var x = document.getElementById("snackbar");
+           x.className = "show";
+           x.innerHTML = "Welcome " + result[1].displayName;
+           setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+           return result[1];  // take result[1] (user details) and pass it to the homepage 
          }
          else{      //failed to log in due to poor connection to database
-		 
-        var x = document.getElementById("snackbar");
-        x.className = "show";
-        x.innerHTML = "Failed to log in due to poor connection to database";
-        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-
+	      var x = document.getElementById("snackbar");
+          x.className = "show";
+          x.innerHTML = "Email Address or Password is incorrect";
+          setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
          }
-     })
-	    }
+	   })  
+    }	 
 }
 
 function validateEmail(email){     //returns true if a string contains only 1 "@"  and at least 1 "."

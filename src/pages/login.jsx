5cf40dc853623173fea0521cf5203921fa1ba2bf
AmaxCoder <78669694+AmaxCoder@users.noreply.github.com>
2022-05-09 14:04:56 +0200

    Commented Login\

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -51,6 +51,7 @@ export default class LoginPage extends React.Component{
     render(){
         return(
             <React.Fragment>
+		{/*creating input fields and tracking changes made to them*/}
                 <h1> Welcome </h1>
                 <form className="form">
                     <div id="snackbar"></div> 

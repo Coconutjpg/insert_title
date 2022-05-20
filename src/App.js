f3dc7d5ced92f837eea2e863cce486d564f076bf
Lord-Y14 <68954094+Lord-Y14@users.noreply.github.com>
2022-05-20 08:00:02 +0200

    Added testid to App.js component

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App" data-testid = "app-1">
         <Router>
           <NavBar/>
           <Routes>

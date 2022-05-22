ee8587f074591a2bdc3cfb3de7a342b783c926fd
dtjabring123 <101052704+dtjabring123@users.noreply.github.com>
2022-05-22 18:45:43 +0200

    Update registration.test.js

--- src/utils/registration.test.js
+++ src/utils/registration.test.js
@@ -2,13 +2,14 @@ import performRegistration from "./registration.js"
 
 //failing registration tests
 describe('failed registration tests',()=>{
-    test('passwords dont match',()=>{
-        expect(performRegistration({firstName : "first name",
+    test('passwords dont match',async ()=>{
+        const output = await performRegistration({firstName : "first name",
         lastName : "surname",
         dob : "22/05/2022",
         emailAddress  :  "email@gmail.com",
         cellNo : "1234567890",
         password : "password",
-        repeatPassword : "passwor"},function(){return 'test'})).toBe({});
+        repeatPassword : "passwor"},function(){return 'test'})
+        expect(output).toBe(false);
     })
 })

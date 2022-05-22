6344de18dc6700f0b78819a35b048d568f1e4f3c
dtjabring123 <101052704+dtjabring123@users.noreply.github.com>
2022-05-22 22:25:05 +0200

    Update recommendations.test.js

--- src/utils/recommendations.test.js
+++ src/utils/recommendations.test.js
@@ -2,6 +2,6 @@ import {get_recommendations} from './recommendations.js'
 
 describe('recommendations test', () =>{
     test('recommendation test', () =>{
-        expect(get_recommendations("general",'1naR0WwJu2JptBUPskhI')).toBe({});
+        expect(get_recommendations("general",'1naR0WwJu2JptBUPskhI')).toStrictEqual({});
     })
 })

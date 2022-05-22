99fa764ffceb1e9eb28c42c925cfb18d32bd35e1
dtjabring123 <101052704+dtjabring123@users.noreply.github.com>
2022-05-22 22:02:13 +0200

    Update recommendations.test.js

--- src/utils/recommendations.test.js
+++ src/utils/recommendations.test.js
@@ -1,8 +1,8 @@
-import get_recommendations from './recommendations'
+import get_recommendations from './recommendations.js'
 
 describe('recommendations test', () =>{
     test('recommendation test', () =>{
-        const output = await get_recommendations("general",null);
+        const output = await get_recommendations("general",'1naR0WwJu2JptBUPskhI');
         expect(output).toBe(false);
     })
 })

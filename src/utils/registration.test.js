9f5b2f42742c1eb21c363c6352a1d8caec0c824e
dtjabring123 <101052704+dtjabring123@users.noreply.github.com>
2022-05-22 18:21:38 +0200

    Add files via upload

---
+++ src/utils/registration.test.js
@@ -0,0 +1,14 @@
+import {performRegisration} from "./registration.js"
+
+//failing registration tests
+describe('failed registration tests',()=>{
+    test('passwords dont match',()=>{
+        expect(performRegisration({firstName : "first name",
+        lastName : "surname",
+        dob : "22/05/2022",
+        emailAddress  :  "email@gmail.com",
+        cellNo : "1234567890",
+        password : "password",
+        repeatPassword : "passwor"},function(){return 'test'})).toBe(false);
+    })
+})

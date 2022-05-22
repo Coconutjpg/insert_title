e46acf363638a6144f70b2d2089023fa708b83eb
dtjabring123 <101052704+dtjabring123@users.noreply.github.com>
2022-05-22 22:31:02 +0200

    Update recommendations.test.js

--- src/utils/recommendations.test.js
+++ src/utils/recommendations.test.js
@@ -1,7 +1,7 @@
 import {get_recommendations} from './recommendations.js'
 
 describe('recommendations test', () =>{
-    test('recommendation test', () =>{
+    test('recommendation test', async () =>{
         const output = await get_recommendations("general",'1naR0WwJu2JptBUPskhI')
         expect(output).toStrictEqual({});
     })

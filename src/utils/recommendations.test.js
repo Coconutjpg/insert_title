87e9086999df29f54797431fec947bc270048c3b
dtjabring123 <101052704+dtjabring123@users.noreply.github.com>
2022-05-30 17:28:20 +0200

    Update recommendations.test.js

--- src/utils/recommendations.test.js
+++ src/utils/recommendations.test.js
@@ -8,8 +8,14 @@ jest.setTimeout(100000);
 
 //tests reccomendations function
 describe('recommendations test', () =>{
-    test('recommendation test', () =>{
-        const output = get_recommendations("general",null)
-        return expect(output).resolves.toStrictEqual({});
+    test('recommendation test', async () =>{
+        try{
+            let output = await get_recommendations("general",null);
+            Promise.resolve(output).then((arr)=>{
+                expect(arr[0]).toBe(false);
+            })
+        } catch (e){
+
+        }
     })
 })

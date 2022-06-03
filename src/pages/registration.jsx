56cdf3e75454229bc3445cdb3504410dbba45f3c
AmaxCoder <78669694+AmaxCoder@users.noreply.github.com>
2022-06-04 00:31:26 +0200

    Removed toLowerCase() call
    Removed toLowerCase() call so that not all the values are converted to lowercase (like the names).

--- src/pages/registration.jsx
+++ src/pages/registration.jsx
@@ -91,7 +91,7 @@ export default class RegistrationPage extends React.Component{
     handleInputChange = (event) => {
         const target = event.target;
         const name = target.name;
-        const value = target.value.toLowerCase();
+        const value = target.value;
         
         this.setState({
             [name]: value,

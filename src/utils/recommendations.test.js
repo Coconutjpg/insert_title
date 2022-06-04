b0c02d99eb98dd38bd1a3edc67ef24bc9a7725e5
dtjabring123 <101052704+dtjabring123@users.noreply.github.com>
2022-06-04 14:11:04 +0200

    Update recommendations.test.js

--- src/utils/recommendations.test.js
+++ src/utils/recommendations.test.js
@@ -3,41 +3,24 @@
  */
 import {detailedSuggestions,getCategoryOf,fetchAsync} from './recommendations.js'
 
-describe('recommendations test', () =>{
-    test('recommendation test', async () =>{
-        try{
-            let output = await detailedSuggestions('1naR0WwJu2JptBUPskhI');
-            Promise.resolve(output).then((arr)=>{
-                expect(arr[0]).toBe(false);
-            })
-        } catch (e){
-
-        }
+describe('detailed suggestions test', () =>{
+    test('test with valid item id',async () =>{
+        const output = await detailedSuggestions('1naR0WwJu2JptBUPskhI');
+        expect(output).toBe(false)
     })
 })
 
-describe('recommendations test', () =>{
-    test('recommendation test', async () =>{
-        try{
-            let output = await getCategoryOf('1naR0WwJu2JptBUPskhI');
-            Promise.resolve(output).then((arr)=>{
-                expect(arr[0]).toBe("C");
-            })
-        } catch (e){
-
-        }
+describe('get actegory test', () =>{
+    test('test with valid item id',async () =>{
+        const output = await getCategoryOf('1naR0WwJu2JptBUPskhI');
+        expect(output).toBe(false)
     })
 })
 
-describe('recommendations test', () =>{
-    test('recommendation test', async () =>{
-        try{
-            let output = await fetchAsync("https://get-sd-cluster.herokuapp.com/getcluster/?point=[1,2,3,4,5,6,7,8,9]");
-            Promise.resolve(output).then((arr)=>{
-                expect(arr[0]).toBe(false);
-            })
-        } catch (e){
-
-        }
+describe('fetch async tests', () =>{
+    test('test with valid url',async () =>{
+        const output = await fetchAsync("https://get-sd-cluster.herokuapp.com/getcluster/?point=[1,2,3,4,5,6,7,8,9]");
+        expect(output).toBe(false)
     })
 })
+

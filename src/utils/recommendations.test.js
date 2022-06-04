38c15609ac494fca4dfd826f3157605abfdaec52
dtjabring123 <101052704+dtjabring123@users.noreply.github.com>
2022-06-04 13:58:38 +0200

    Update recommendations.test.js

--- src/utils/recommendations.test.js
+++ src/utils/recommendations.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment node
  */
-import {detailedSuggestions,getCategoryOf} from './recommendations.js'
+import {detailedSuggestions,getCategoryOf,fetchAsync} from './recommendations.js'
 
 describe('recommendations test', () =>{
     test('recommendation test', async () =>{
@@ -22,6 +22,19 @@ describe('recommendations test', () =>{
             let output = await getCategoryOf('1naR0WwJu2JptBUPskhI');
             Promise.resolve(output).then((arr)=>{
                 expect(arr[0]).toBe("C");
+            })
+        } catch (e){
+
+        }
+    })
+})
+
+describe('recommendations test', () =>{
+    test('recommendation test', async () =>{
+        try{
+            let output = await fetchAsync("https://get-sd-cluster.herokuapp.com/getcluster/?point=[1,2,3,4,5,6,7,8,9]");
+            Promise.resolve(output).then((arr)=>{
+                expect(arr[0]).toBe(false);
             })
         } catch (e){
 

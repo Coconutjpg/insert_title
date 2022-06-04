305840855997e374eb85174be50af53574eedca2
dtjabring123 <101052704+dtjabring123@users.noreply.github.com>
2022-06-04 11:30:09 +0200

    Create recommendations.test.js

---
+++ src/utils/recommendations.test.js
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment node
+ */
+import {detailedSuggestions} from './recommendations.js'
+
+describe('recommendations test', () =>{
+    test('recommendation test', async () =>{
+        try{
+            let output = await detailedSuggestions('1naR0WwJu2JptBUPskhI');
+            Promise.resolve(output).then((arr)=>{
+                expect(arr[0]).toBe(false);
+            })
+        } catch (e){
+
+        }
+    })
+})

489789da471ff7ecaad750cd02659f7670af5483
dtjabring123 <101052704+dtjabring123@users.noreply.github.com>
2022-06-05 14:04:32 +0200

    Update recommendations.test.js

--- src/utils/recommendations.test.js
+++ src/utils/recommendations.test.js
@@ -7,10 +7,10 @@ global.fetch = jest.fn(() => Promise.resolve({
     json: () => Promise.resolve([])
 }));
 
-describe('get recommendations test', () =>{
-    test('test with valid type and item id',async () =>{
-        const output = await get_recommendations("general",null);
-        expect(output).toBe([])
+describe('get category test', () =>{
+    test('test with valid item id',async () =>{
+        const output = await getCategoryOf('1naR0WwJu2JptBUPskhI');
+        expect(output).toBe("Chassis")
     })
 })
 
@@ -21,18 +21,18 @@ describe('detailed suggestions test', () =>{
     })
 })
 
-describe('get category test', () =>{
-    test('test with valid item id',async () =>{
-        const output = await getCategoryOf('1naR0WwJu2JptBUPskhI');
-        expect(output).toBe("Chassis")
-    })
-})
-
 describe('fetch async tests', () =>{
     test('test with valid url',async () =>{
         const output = await fetchAsync("https://get-sd-cluster.herokuapp.com/getcluster/?point=[1,2,3,4,5,6,7,8,9,10]");
         expect(output).toBe([]);
         //expect(fetch).toHaveBeenCalledWith("https://get-sd-cluster.herokuapp.com/getcluster/?point=[1,2,3,4,5,6,7,8,9]");
+    })
+})
+
+describe('get recommendations test', () =>{
+    test('test with valid type and item id',async () =>{
+        const output = await get_recommendations("general",null);
+        expect(output).toBe([])
     })
 })
 
